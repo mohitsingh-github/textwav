487982d4a08d7d7f861adea25f5897f4666196c2
mohitsingh-github <73215107+mohitsingh-github@users.noreply.github.com>
2020-12-02 11:48:56 +0530

    a

---
+++ homepage/script.js
@@ -0,0 +1,16 @@
+var a=0;
+var b=0;
+
+function submit(event) {
+	var a = document.getElementById("un").value;
+	var b = document.getElementById("pw").value;
+	if(b=="Mohit"){
+console.log("redirecting to profile")
+}
+else{
+	document.getElementById("loginmsg") .textContent = "wrong password";
+}
+
+}
+
+
