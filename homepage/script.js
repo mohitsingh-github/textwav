87e3033cd3f2d7cb8c9a667a68d4693df00935ad
mohitsingh-github <73215107+mohitsingh-github@users.noreply.github.com>
2020-12-03 21:12:56 +0530

    a

--- homepage/script.js
+++ homepage/script.js
@@ -1,15 +1,24 @@
-var a=0;
-var b=0;
+ let users = [];
+        const adduser = (ev)=>{
+            ev.preventDefault();  //to stop the form submitting
+            let user = {
+                id: Date.now(),
+                name: document.getElementById('name').value,
+                username: document.getElementById('username').value,
+                password: document.getElementById('password').value
+            }
+            users.push(user);
+            document.forms[0].reset(); // to clear the form for the next entries
+            //document.querySelector('form').reset();
 
-function submit(event) {
-	var a = document.getElementById("un").value;
-	var b = document.getElementById("pw").value;
-	if(b=="Mohit"){
-console.log("redirecting to profile")
-}
-else{
-	document.getElementById("loginmsg") .textContent = "wrong password";
+            //for display purposes only
+            console.warn('added' , {users} );
+            let pre = document.querySelector('#msg pre');
+            pre.textContent = '\n' + JSON.stringify(users, '\t', 2);
 
-}
-
-}
+            //saving to localStorage
+            localStorage.setItem('userslist', JSON.stringify(users) );
+        }
+        document.addEventListener('DOMContentLoaded', ()=>{
+            document.getElementById('registerbtn').addEventListener('click', adduser);
+        });
