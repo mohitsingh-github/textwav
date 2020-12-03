5072b46a3b234a1e5005f50b1da930347feea0ac
mohitsingh-github <73215107+mohitsingh-github@users.noreply.github.com>
2020-12-04 05:07:34 +0530

    a

--- homepage/script.js
+++ homepage/script.js
@@ -3,16 +3,28 @@
  function login() {
     var usernamelogin = document.getElementById("usernamelogin").value;
     var passwordlogin = document.getElementById("passwordlogin").value;
-    console.log(usernamelogin);
-    console.log(passwordlogin);
-}
 
+if(usernamelogin && passwordlogin){
+        for(let i = 0; i < localStorage.length; i++){
+            if(usernamelogin == localStorage.username && passwordlogin == localStorage.password)
+            {
+                 alert(localStorage.name+" is successfully logged in");
+            }
+        }
+}
+}
 
  function register() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+        var cpassword = document.getElementById("cpassword").value;
     var name = document.getElementById("name").value;
-    console.log(username);
-    console.log(password);
-    console.log(name);
+    
+if(username && password && name){
+if(password==cpassword){
+        localStorage.setItem("username", username);
+        localStorage.setItem("password", password);
+                localStorage.setItem("name", name);
+}
+}
 }
