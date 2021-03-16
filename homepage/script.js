6c00fcf40d6dd7fd3055a2adddbfb9a36bd1888d
mohitsingh-github <73215107+mohitsingh-github@users.noreply.github.com>
2021-03-17 02:59:31 +0530

    a

---
+++ homepage/script.js
@@ -0,0 +1,9 @@
+function showr(){
+	document.getElementById("login-form").style.display="none";
+		document.getElementById("register-form").style.display="block";
+}
+function showl(){
+	document.getElementById("register-form").style.display="none";
+		document.getElementById("login-form").style.display="block";
+}
+
