33052e5a620ed00de6c24ad132ea3268bdc5f5f1
mohitsingh-github <mohit1710158@akgec.ac.in>
2021-06-30 00:23:24 +0530

    a

--- homepage/public/script.js
+++ homepage/public/script.js
@@ -10,13 +10,3 @@ function showl(){
 	document.getElementById("register-form").style.display="none";
 		document.getElementById("login-form").style.display="block";
 }
-
-//registration
-document.getElementById("registerbtn").addEventListener("click", function() {
-  document.getElementById("registermsg").innerHTML = "Registration Attempted";
-});
-
-//login
-document.getElementById("loginbtn").addEventListener("click", function() {
-  document.getElementById("loginmsg").innerHTML = "Login Attempted";
-});
